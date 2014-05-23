fba5d5865ee79a65bf16bce8455830fbad37b627
Michael Tseng <matseng@gmail.com>
2014-05-22 22:19:04 -0700

    Used  or ng-init to initialize value of placeholder attribute, and used ng conditional expressions in DOM

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ angular.module('ngMadLibs', []);
 
 angular.module('ngMadLibs')
   .controller('MadLibsController', ['$scope', function($scope) {
-    $scope.maleName = {text: '', placeholder: 'male name'};
-    // $scope.maleName.text = '';
-    // $scope.maleName.placeholder = 'male name';
+    // $scope.maleName = {text: '', placeholder: 'male name'};
+    $scope.maleName = {placeholder: 'male name'};
   }]);
