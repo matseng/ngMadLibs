e176d75fae84f7880309218d1c925a26eebd5677
Michael Tseng <matseng@gmail.com>
2014-05-31 10:40:32 -0700

    Used  to broadcast events btw sibling scopes. TODO: Refactor paragraph DOM to use simple 2-way data binding

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 angular.module('ngMadLibs', []);
 
 angular.module('ngMadLibs')
-  .controller('MadLibsController', ['$scope', function($scope) {
-    // $scope.maleName = {text: '', placeholder: 'male name'};
-    $scope.data = {};
-    $scope.data.personName = {placeholder: 'person name'};
-    $scope.data.tediousTask = {placeholder: 'tedious task'};
-    $scope.dirtyTask = {placeholder: 'dirty task'};
-    $scope.celebrity = {placeholder: 'celebrity'};
-    $scope.uselessSkill = {placeholder: 'useless skill'};
-    $scope.obnoxiousCelebrity = {placeholder: 'obnoxious celebrity'};
-    $scope.hugeNumber = {placeholder: 'huge number'};
-    $scope.adjective = {placeholder: 'adjective'};
+  .controller('FormController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    
+    function initializeData() {
+      $scope.data = {};
+      $scope.data.gender = 'Other';
+      $scope.data.personName = {placeholder: 'person name'};
+      $scope.data.jobTitle = {placeholder: 'job title'};
+      $scope.data.tediousTask = {placeholder: 'tedious task'};
+      $scope.dirtyTask = {placeholder: 'dirty task'};
+      $scope.celebrity = {placeholder: 'celebrity'};
+      $scope.uselessSkill = {placeholder: 'useless skill'};
+      $scope.obnoxiousCelebrity = {placeholder: 'obnoxious celebrity'};
+      $scope.hugeNumber = {placeholder: 'huge number'};
+      $scope.adjective = {placeholder: 'adjective'};
+    };
+    initializeData();
 
-    $scope.gender = 'Other';
     var genderLookup = {
       Other: {
         pronoun: 'he/she',
@@ -28,17 +32,39 @@ angular.module('ngMadLibs')
         possessive: 'his'
       }
     };
+
     $scope.$watch('data.gender', function() {
-      $scope.pronoun = genderLookup[$scope.gender].pronoun;
-      $scope.possessive = genderLookup[$scope.gender].possessive;
+      $scope.pronoun = genderLookup[$scope.data.gender].pronoun;
+      $scope.possessive = genderLookup[$scope.data.gender].possessive;
+    });
+
+    $scope.submit = function(a,b,c) {
+      $scope.MyForm.submitted = true;
+      // console.log($scope.data);
+      console.log($scope.MyForm);
+      
+      if ($scope.MyForm.$valid) {
+        $rootScope.$broadcast('formSubmitted', $scope.data);
+      }
+    };
+
+    $scope.$on('resetClicked', function() {
+      console.log("hello world, reset 2");
+      initializeData();
+      $scope.data.gender = 'F';  //TODO: remove, just for testing
     });
   }]);
 
 angular.module('ngMadLibs')
-  .controller('FormController', ['$scope', function($scope) {
-    $scope.submit = function(a,b,c) {
-      $scope.MyForm.submitted = true;
+  .controller('ParagraphController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    
+    $scope.$on('formSubmitted', function(event, formData) {
+      $scope.data = formData;
       console.log($scope.data);
+    })
+
+    $scope.reset = function() {
+      $rootScope.$broadcast('resetClicked');
     };
   }]);
 
@@ -57,8 +83,8 @@ angular.module('ngMadLibs')
         var wordString = $wordElement.attr('data-word');
         if(wordString) {
           var wordObject = $wordElement.scope()[wordString];
-          var wordClassExpression = '{placeholder: !(' + wordString + '.text)}';
-          var wordTextExpression = "{{" + wordString + ".text || '[' + " + wordString + ".placeholder + ']'}}";
+          var wordClassExpression = '{placeholder: !( data[' + wordString + '].text)}';
+          var wordTextExpression = "{{ data[" + wordString + "].text || '[' + data[" + wordString + "].placeholder + ']'}}";
           $wordElement.attr('ng-class', wordClassExpression);
           $wordElement.html(wordTextExpression);
           $compile($wordElement)($scope);
